c412003516ff166b46c8066305d965b705c5e972
Roni Sundström <s2401255@edu.bc.fi>
2026-01-29 11:39:04 +0200

    karlson

--- Assets/Scripts/StartBlocker.cs
+++ Assets/Scripts/StartBlocker.cs
@@ -5,6 +5,7 @@ public class StartBlocker : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+
         
     }
 
